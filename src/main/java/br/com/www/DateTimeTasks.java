ab270133670c8f6d0f64778be4b5fcbe1b09da3c
Eduardo Ferreira <efbomfilho@yahoo.com>
2026-03-03 14:24:58 -0300

    (1)Adicionei um exercicio de LocalDateTime

---
+++ src/main/java/br/com/www/DateTimeTasks.java
@@ -0,0 +1,25 @@
+package br.com.www;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeTasks {
+    public static void main(String[] args) {
+        String task = "Send weekly report";
+
+        LocalDate dateTask = LocalDate.now();
+        LocalTime timeTask = LocalTime.now();
+
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dateTaskFormatted = dateTask.format(formatDate);
+
+        DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String timeTaskFormatted = timeTask.format(formatTime);
+
+        System.out.printf("""
+                %nTASK: %s
+                Registration date: %s
+                Registration time: %s%n""", task, dateTaskFormatted, timeTaskFormatted);
+    }
+}

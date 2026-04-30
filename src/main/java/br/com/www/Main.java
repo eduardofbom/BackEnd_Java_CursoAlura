7dafb889c5949851c99b751a7ea47e878288a679
Eduardo Ferreira <efbomfilho@yahoo.com>
2026-04-30 09:59:11 -0300

    Adicionei mais uma FunctionalInterface para adaptar a operação para Double e usar no main. Finalizei os exercícios de funções lambda

--- src/main/java/br/com/www/Main.java
+++ src/main/java/br/com/www/Main.java
@@ -1,13 +1,32 @@
 package br.com.www;
 
-import br.com.www.interfaces.ICheck;
-import br.com.www.interfaces.IOperation;
+import br.com.www.interfaces.IOperationDouble;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        IOperationDouble division = (n1, n2) -> {
+            if(n2 == 0) {
+                 throw new ArithmeticException("The divisor is 0 (zero).");
+            }
+            return n1 / n2;
+        };
 
+        try {
+            System.out.println(division.execute(10, 2));
+            System.out.println(division.execute(10, 0));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
+
+
+
+
+/*
+7 - Crie uma função lambda que recebe dois números e divide o primeiro pelo segundo. A função deve lançar uma exceção
+    de tipo ArithmeticException se o divisor for zero.
+ */
---
+++ src/main/java/br/com/www/interfaces/IOperationDouble.java
@@ -0,0 +1,6 @@
+package br.com.www.interfaces;
+
+@FunctionalInterface
+public interface IOperationDouble {
+    double execute(double a, double b);
+}
